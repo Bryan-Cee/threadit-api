440a528f4cf2530e182d59f99d31d57203366fce
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-10-12 02:03:58 +0300

    Simple user creation setup

---
+++ knex/migrations/20191011204214_create_user_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', function (t) {
+        t.increments('id').unsigned().primary();
+        t.string('username').notNull();
+        t.text('firstname').notNull(); 
+        t.text('lastname').notNull();
+        t.text('avatar').nullable();
+        t.text('password').notNull();
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+};
