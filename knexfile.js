87695e21e418af2eb3ff85c279c6a78b1c995d0d
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-10-11 11:56:59 +0300

    setup knex for database management
    Knex will be used to manage the database and migrations
    as well as seeding the database.

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: `${__dirname}/knex/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/knex/seeds`,
+    },
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: `${__dirname}/knex/migrations`,
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: `${__dirname}/knex/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/knex/seeds`,
+    },
+  },
+};
