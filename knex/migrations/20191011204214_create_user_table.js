cc2a501a86577d8b30ffabfbdae0956218c78a32
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-10-12 03:35:51 +0300

    set up linting
    linting has been set up using stardard js and commonJS
    starndards for node.

--- knex/migrations/20191011204214_create_user_table.js
+++ knex/migrations/20191011204214_create_user_table.js
@@ -1,15 +1,15 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable('users', function (t) {
-        t.increments('id').unsigned().primary();
-        t.string('username').notNull();
-        t.text('firstname').notNull(); 
-        t.text('lastname').notNull();
-        t.text('avatar').nullable();
-        t.text('password').notNull();
-    });
+  return knex.schema.createTable("users", function(t) {
+    t.increments("id").unsigned().primary();
+    t.string("username").notNull();
+    t.text("firstname").notNull();
+    t.text("lastname").notNull();
+    t.text("avatar").nullable();
+    t.text("password").notNull();
+  });
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTable('users');
+  return knex.schema.dropTable("users");
 };
