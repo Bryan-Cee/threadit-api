69a55009a6f24525a64708fd13d959e7dcc9d475
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-10-22 21:37:13 +0300

    feat: add create community
    Users can now create communities they want.
    
    Other changes made by this commit include:
     - Error handling for database queries should be written
     in the datasources module
     - Moved the profileInput input to the profile.graphql file
     - updated the name for communities to unique
    
    issue_id #5

--- knex/migrations/20191022192221_community.js
+++ knex/migrations/20191022192221_community.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("communities", function(t) {
     t.bigIncrements("community_id").primary().unsigned();
-    t.string("name").nullable();
+    t.string("name").unique().notNullable();
     t.text("description").nullable();
     t.timestamps(false, true);
   }).then(() => console.log("=> Communities table created..."));
