9583d7d7d43250ad984a45393bf7d697ce0c4ed9
Bryan-Cee <cheruiyotbryan@gmail.com>
2020-01-01 20:35:17 +0300

    chore: fix env in production

--- knex/index.js
+++ knex/index.js
@@ -1,4 +1,4 @@
-require("dotenv-safe").config();
+require("dotenv-safe").config({ allowEmptyValues: true });
 
 const NODE_ENV = process.env.NODE_ENV;
 const config = require("../knexfile")[NODE_ENV];
